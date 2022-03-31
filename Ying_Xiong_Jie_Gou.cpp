5ef2982479d355b4edc92d7e384501faf5ffa044
Cahr-bit <cahr5513@163.com>
2022-03-31 21:03:57 +0800

    master

--- Ying_Xiong_Jie_Gou.cpp
+++ Ying_Xiong_Jie_Gou.cpp
@@ -14,9 +14,9 @@ struct hero
 
 void bubblesort(hero arr[], int len)
 {
-	for (int i = 0; i < len; i++)
+	for (int i = 0; i < len - 1; i++)
 	{
-		for (int j = 0; j <len - i; j++)
+		for (int j = 0; j <len - 1 - i; j++)
 		
 		{
 			if (arr[j].age > arr[j+1].age)
